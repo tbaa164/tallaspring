e195050e942147d14d9f1fdf2871eff91e0d7f2c
tbaa164 <tbaa164>
2024-03-29 15:32:59 +0100

    deuxieme commit : ajout du code initial

--- src/main/java/com/example/demo/SbCarApplication.java
+++ src/main/java/com/example/demo/SbCarApplication.java
@@ -1,13 +1,34 @@
 package com.example.demo;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-public class DemoApplication {
+public class SbCarApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
-	}
+    private static final Logger logger = LoggerFactory.getLogger(SbCarApplication.class);
 
+    public static void main(String[] args) {
+        SpringApplication.run(SbCarApplication.class, args);
+        logger.info("Car App started! Goto http://localhost:8081/dic_student_name");
+    }
+}
+
+@RestController
+@RequestMapping("/dic_student_name")
+class SbCarController {
+
+    @Value("${fullname: Bass}")
+    private String fullname;
+
+    @GetMapping
+    public String getFullname() {
+        return "Work done by " + this.fullname + "!";
+    }
 }
